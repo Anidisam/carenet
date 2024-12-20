9beb7ec039f4688d2b72ca8f41ac9dd969ba3b3f
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2024-12-20 14:47:57 -0800

    started navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../assets/css/navbar.css";
+
+const Navbar = () => {
+  return (
+<nav className="navbar">
+    <div className="navbar-logo">Carenet</div>
+</nav>
+  );
+};
+
+export default Navbar;
