7a12cf1a4cd5fc77a86a21119a659a94846e54d1
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2025-02-27 00:00:25 -0800

    working

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
+import React from "react";
 import "../assets/css/download.css";
 import playstore from "../assets/images/playstore.svg";
 import Ios from "../assets/images/applestore.svg";
+import backgroundImage from "../assets/images/background.svg"; // Import image
 
 const Download = () => {
   return (
-    <div className="download-app-section">
+    <div
+      className="download-app-section"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <h3>Download Our App</h3>
       <h5>
-        Get access to all our features. Start <br />interacting with over 500 medical <br />
+        Get access to all our features. Start <br />
+        interacting with over 500 medical <br />
         professionals
       </h5>
 
       <div className="download-buttons">
         <button className="download-btn">
-            <span>Android</span>
-            <img src={playstore} alt="playstore" />
+          <span>Android</span>
+          <img src={playstore} alt="playstore" />
         </button>
 
-        <button className="download-btn">
-            <span>Ios</span>
-            <img src={Ios} alt="Apple Store" />
+        <button className="download-btn2">
+          <span>Ios</span>
+          <img src={Ios} alt="Apple Store" />
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default Download
+export default Download;
