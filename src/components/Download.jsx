012daf6d105454dc57bfe1797fbeccc50f6d5bcc
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2025-04-01 02:21:23 -0700

    almost done

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,32 +1,68 @@
 import React from "react";
 import "../assets/css/download.css";
-import playstore from "../assets/images/playstore.svg";
-import Ios from "../assets/images/applestore.svg";
-import backgroundImage from "../assets/images/background.svg"; // Import image
+import mainImg from "../assets/images/girl phone.svg";
+import img1 from "../assets/images/girl phone.svg";
+import img2 from "../assets/images/boy dna.svg";
+import img3 from "../assets/images/testi 2.svg";
+import img4 from "../assets/images/testi 3.svg";
+
+const imageData = [
+  {
+    img: img1,
+    title: "Download our app now",
+    description:
+      "Get access to all our features. Start ",
+  },
+  {
+    img: img2,
+    title: "24/7 Telemedicine",
+    description:
+      "Doctors are waiting to listen to you. ",
+  },
+  {
+    img: img3,
+    title: "Got questions? Want to know",
+    description:
+      "Get on the timeline, ask questions",
+  },
+  {
+    img: img4,
+    title: "Secure system and cutting-edge ",
+    description:
+      "Our systems are secured to protect you .",
+  },
+];
 
 const Download = () => {
   return (
-    <div
-      className="download-app-section"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <h3>Download Our App</h3>
-      <h5>
-        Get access to all our features. Start <br />
-        interacting with over 500 medical <br />
-        professionals
-      </h5>
+    <div className="download-section">
+      <h1 className="section-title">Ready to explore? We got you covered</h1>
 
-      <div className="download-buttons">
-        <button className="download-btn">
-          <span>Android</span>
-          <img src={playstore} alt="playstore" />
-        </button>
+      <div className="download-container">
+        {/* Main Image (Left Side) */}
+        <div className="main-image">
+          <img src={mainImg} alt="Main App" />
+          <div className="image-content">
+            <h1>About LAGIS</h1>
+            <p>
+              Get access to all our features. Start interacting with over 500
+              medical professionals.
+            </p>
+          </div>
+        </div>
 
-        <button className="download-btn2">
-          <span>Ios</span>
-          <img src={Ios} alt="Apple Store" />
-        </button>
+        {/* Stacked Images (Right Side) */}
+        <div className="stacked-images">
+          {imageData.map((item, index) => (
+            <div key={index} className="stacked-image">
+              <img src={item.img} alt={item.title} />
+              <div className="image-content">
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
